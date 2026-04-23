72d34aec083d7699574b05b4b813ff4a8be1a4ff
turpah86 <turpah86@gmail.com>
2026-04-23 10:18:19 +0900

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -2,34 +2,34 @@
     'use strict';
 
     function startPlugin() {
-        // Показываем сообщение при запуске
-        Lampa.Noty.show('Плагин Anime успешно загружен!');
+        // 1. Сразу выводим уведомление - если его нет, значит файл не запустился
+        Lampa.Noty.show('Аниме плагин: ПРОВЕРКА ЗАПУСКА');
 
-        // Добавляем новый компонент (страницу)
-        Lampa.Component.add('my_anime', function (object) {
+        // 2. Создаем страницу
+        Lampa.Component.add('my_anime_page', function (object) {
             var comp = new Lampa.InteractionMain(object);
             comp.create = function () {
-                return '<div style="padding: 2rem; font-size: 1.5rem;">Добро пожаловать в твой личный аниме-плагин! Скоро здесь будут списки серий.</div>';
+                return '<div style="padding: 50px; font-size: 24px; color: #fff;">УРА! Твой личный плагин открылся!</div>';
             };
             return comp;
         });
 
-        // Добавляем кнопку в левое меню Лампы
-        Lampa.Menu.add({
-            id: 'my_anime',
-            title: 'Anime Pro',
-            icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://w3.org"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" fill="white"/></svg>',
-            component: 'my_anime', // ЭТО ВАЖНО: имя должно совпадать с Lampa.Component.add('my_anime', ...)
-            ordered: 1
-        });
-
-    }
-
-    // Запуск при готовности приложения
-    if (window.appready) startPlugin();
-    else {
+        // 3. Добавляем в меню (самый простой способ)
         Lampa.Listener.follow('app', function (e) {
-            if (e.type == 'ready') startPlugin();
+            if (e.type == 'ready') {
+                Lampa.Menu.add({
+                    id: 'my_anime_page',
+                    title: 'Anime Pro',
+                    icon: '<svg height="36" viewBox="0 0 24 24" width="36" fill="white"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2z"/></svg>',
+                    component: 'my_anime_page',
+                    ordered: 0 // Ставим в самый верх меню
+                });
+            }
         });
     }
+
+    if (window.appready) startPlugin();
+    else Lampa.Listener.follow('app', function (e) {
+        if (e.type == 'ready') startPlugin();
+    });
 })();
