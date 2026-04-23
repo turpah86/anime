b32ffa613295c8dfc86bca90ceddb5267b78f0ea
turpah86 <turpah86@gmail.com>
2026-04-23 10:27:46 +0900

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -2,36 +2,39 @@
     'use strict';
 
     function startPlugin() {
-        // Добавляем компонент (страницу) как это делают профи
-        Lampa.Component.add('anime_pro', function (object) {
+        // 1. Создаем страницу нашего плагина
+        Lampa.Component.add('my_custom_anime', function (object) {
             var comp = new Lampa.InteractionMain(object);
-            
             comp.create = function () {
-                // Это создаст пустую сетку, куда потом можно грузить постеры
-                return comp.render();
+                return '<div style="padding: 100px; text-align: center;"><h1>🚀 Твой личный плагин работает!</h1><p>Если ты видишь это, значит мы победили кеш.</p></div>';
             };
-
-            comp.empty = function () {
-                return '<div class="empty">Здесь скоро будет список аниме...</div>';
-            };
-
             return comp;
         });
 
-        // "Спизженный" метод регистрации меню, который работает во всех версиях
-        var item = {
-            id: 'anime_pro',
-            title: 'Anime Pro',
-            icon: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://w3.org"><path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20Z" fill="white"/></svg>',
-            component: 'anime_pro',
-            ordered: 1
-        };
+        // 2. Функция добавления кнопки
+        function addToMenu() {
+            var menu_item = {
+                id: 'my_custom_anime',
+                title: 'МОЁ АНИМЕ ⚡️', // Название специально другое!
+                icon: '<svg height="36" viewBox="0 0 24 24" width="36" fill="#ff0000"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2z"/></svg>',
+                component: 'my_custom_anime',
+                ordered: 0 // Ставим в самый верх
+            };
 
-        // Принудительная вставка в список меню
-        Lampa.Menu.add(item);
+            // Добавляем в список меню
+            Lampa.Menu.add(menu_item);
+            
+            // Форсируем обновление интерфейса
+            Lampa.Component.updateMenu();
+        }
+
+        // Запускаем добавление
+        addToMenu();
         
-        // Уведомление для проверки
-        Lampa.Noty.show('Anime Pro: Плагин активирован!');
+        // Повторяем через 2 секунды (на случай если Лампа тормозит)
+        setTimeout(addToMenu, 2000);
+
+        Lampa.Noty.show('Плагин МОЁ АНИМЕ загружен!');
     }
 
     if (window.appready) startPlugin();
