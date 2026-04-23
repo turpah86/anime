e8c16e6b015c6268db5e968709b9e7466e41a10b
turpah86 <turpah86@gmail.com>
2026-04-23 10:51:48 +0900

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -8,24 +8,29 @@
             this.activity.loader(true);
             var _this = this;
 
-            // Тянем данные из открытой базы (как это делают все аниме-плагины)
+            // ТЕСТОВЫЕ ДАННЫЕ (если интернет-база тупит)
+            var test_items = [
+                {title: 'Наруто', img: 'https://media-amazon.com', description: 'Тестовая карточка'},
+                {title: 'Ван Пис', img: 'https://media-amazon.com', description: 'Проверка работы'}
+            ];
+
+            // Пробуем загрузить реальный ТОП аниме
             Lampa.HTTP.get('https://jikan.moe', function (data) {
                 var items = data.data.map(function (a) {
                     return {
                         title: a.title,
                         name: a.title,
                         img: a.images.jpg.image_url,
-                        background_image: a.images.jpg.large_image_url,
-                        description: a.synopsis,
-                        year: a.year,
-                        rating: a.score
+                        description: a.synopsis
                     };
                 });
-
                 _this.build(items);
                 _this.activity.loader(false);
             }, function () {
-                Lampa.Noty.show('Ошибка загрузки базы аниме');
+                // Если база не ответила - грузим тест
+                _this.build(test_items);
+                _this.activity.loader(false);
+                Lampa.Noty.show('Гружу тестовые данные...');
             });
 
             return comp.render();
@@ -35,22 +40,9 @@
             var _this = this;
             items.forEach(function(item) {
                 var card = Lampa.Template.get('card', item);
-                
-                card.on('hover:focus', function () {
-                    Lampa.Background.change(item.background_image);
-                });
-
                 card.on('hover:enter', function () {
-                    // При клике открываем встроенный поиск Лампы по этому аниме
-                    Lampa.Activity.push({
-                        url: '',
-                        title: 'Поиск: ' + item.title,
-                        component: 'search',
-                        search: item.title,
-                        page: 1
-                    });
+                    Lampa.Activity.push({url: '', title: item.title, component: 'search', search: item.title, page: 1});
                 });
-
                 _this.append(card);
             });
         };
@@ -58,7 +50,6 @@
         return comp;
     });
 
-    // Вставляем кнопку в меню (метод, который у тебя сработал)
     function inject() {
         var menu = $('.menu .scroll__body');
         if (menu.length && !$('.menu__item[data-action="my_hacker_anime"]').length) {
@@ -66,14 +57,11 @@
                 '<div class="menu__ico"><svg height="36" viewBox="0 0 24 24" width="36" fill="#00ff00"><path d="M13 10V3L4 14h7v7l9-11h-7z"/></svg></div>' +
                 '<div class="menu__text">ХАКЕР АНИМЕ ⚡️</div>' +
             '</div>');
-
             item.on('click', function () {
                 Lampa.Activity.push({ title: 'ХАКЕР АНИМЕ', component: 'my_hacker_anime' });
             });
-
             menu.append(item);
         }
     }
-
     setInterval(inject, 2000);
 })();
