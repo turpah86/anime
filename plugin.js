0a64e8c244a7bec190cca69944de9515f1357eeb
turpah86 <turpah86@gmail.com>
2026-04-23 10:15:54 +0900

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -18,9 +18,11 @@
         Lampa.Menu.add({
             id: 'my_anime',
             title: 'Anime Pro',
-            icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://w3.org"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>',
+            icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://w3.org"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" fill="white"/></svg>',
+            component: 'my_anime', // ЭТО ВАЖНО: имя должно совпадать с Lampa.Component.add('my_anime', ...)
             ordered: 1
         });
+
     }
 
     // Запуск при готовности приложения
